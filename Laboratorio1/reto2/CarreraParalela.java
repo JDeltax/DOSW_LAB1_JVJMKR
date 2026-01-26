33542277c3e3608d7980e8c9f04f5df169f9ead4
JDeltax <vegavillamiljuanpablo@gmail.com>
2026-01-26 18:46:51 -0500

    Funcion combinada! en feature reto 2

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -3,43 +3,100 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+
+
 public class CarreraParalela {
+
     public void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         String numbers = scanner.nextLine();
-        
         int elnum = maxnum(numbers);
-        
         List<Integer> array = convertToList(numbers);
         int small = smallNumber(array);
         int lenNumbers = array.size();
-        
         System.out.println(elnum);
         System.out.println(small);
         System.out.println(lenNumbers);
+	
     }
 
+
+
     public List<Integer> convertToList(String numbers) {
+
         return Arrays.stream(numbers.split(" "))
+
                      .map(Integer::parseInt)
+
                      .toList();
+
     }
 
     public int smallNumber(List<Integer> array) {
+
         return array.stream()
+
                       .min(Integer::compare)
+
                       .get();
+
     }
 
     public static int elUnum;
 
+
+
     public static int maxnum(String numeros){
-        List<Integer> lista = Arrays.stream(numeros.split(" ")) 
+
+        List<Integer> lista = Arrays.stream(numeros.split(" "))
+
                                     .map( x -> Integer.parseInt(x))
+
                                     .collect(Collectors.toList());
+
+
 
         int max = lista.stream().max(Integer::compareTo).orElse(0);
 
+
+
         return max;
+
     }
+
+        List<Integer> lista = Arrays.stream(numeros.split(" "))
+
+        Resultado resultix = new Resultado(max, min, lista.size());
+        return resultix;
+
+
+    }
+
+
+class Resultado {
+    int min;
+    int cantidad;
+
+        this.max = max;
+        this.min = min;
+    }
+
+    @Override
+    public String toString() {
+    }
+}        return "FINAL -> Máximo: " + max + ", Mínimo: " + min + ", Total datos: " + cantidad;
+        this.cantidad = cantidad;
+    public Resultado(int max, int min, int cantidad) {
+    int max;
 }
+
+
+
+        int min =lista.stream().min(Integer::compareTo).get();
+        int max = lista.stream().max(Integer::compareTo).orElse(0);
+                                    .map(x -> Integer.parseInt(x))
+                                    .collect(Collectors.toList());
+
+
+
