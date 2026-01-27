afc8726a56343f4f5ae5071c99728495635e8cb8
Daniel Moreno <danieldaleman@hotmail.com>
2026-01-26 19:02:03 -0500

    feature: funcion combinada

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -3,26 +3,33 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+
+
 public class CarreraParalela {
+
     public void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         String numbers = scanner.nextLine();
-        
         int elnum = maxnum(numbers);
-        
         List<Integer> array = convertToList(numbers);
         int small = smallNumber(array);
         int lenNumbers = array.size();
-        
         System.out.println(elnum);
         System.out.println(small);
         System.out.println(lenNumbers);
+
+        Result resultFinal = finalResult(numbers);
+        System.out.println(resultFinal.toString());
+
     }
 
     public List<Integer> convertToList(String numbers) {
+
         return Arrays.stream(numbers.split(" "))
                      .map(Integer::parseInt)
                      .toList();
+
     }
 
     public int smallNumber(List<Integer> array) {
@@ -34,12 +41,44 @@ public class CarreraParalela {
     public static int elUnum;
 
     public static int maxnum(String numeros){
-        List<Integer> lista = Arrays.stream(numeros.split(" ")) 
+        List<Integer> lista = Arrays.stream(numeros.split(" "))
+
                                     .map( x -> Integer.parseInt(x))
                                     .collect(Collectors.toList());
 
         int max = lista.stream().max(Integer::compareTo).orElse(0);
-
         return max;
+
+    }
+
+
+    public static Result finalResult(String numbers){
+
+        List<Integer> listNumbers = Arrays.stream(numbers.split(" "))
+                                    .map(x -> Integer.parseInt(x))
+                                    .collect(Collectors.toList());
+        int max = listNumbers.stream().max(Integer::compareTo).orElse(0);
+        int min =listNumbers.stream().min(Integer::compareTo).get();
+        
+        Result resultix = new Result(max, min, lista.size());
+        return resultix;
+    }
+
+}
+
+class Result {
+    int max;
+    int min;
+    int cantidad;
+
+    public Resultado(int max, int min, int cantidad) {
+        this.max = max;
+        this.min = min;
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Max: " + max + ", Mín: " + min + ", long: " + cantidad;
     }
 }
